0e2d85576bd816055ce133eee48aeae92fc0c2f5
naveenverma005 <82157280+naveenverma005@users.noreply.github.com>
2021-04-22 14:34:45 +0530

    Create variables.tf

---
+++ modules/igw/variables.tf
@@ -0,0 +1,10 @@
+# Variables for Internet Gateway
+variable "internetGateWayName" {
+  type        = string
+  default     = "IGW"
+  description = "This is my Internet GateWay name"
+}
+
+variable "vpc_id" {
+
+}
