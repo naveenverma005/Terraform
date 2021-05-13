7de7b6b33eeb695ecef21c14bbb959c1f1c4c832
naveenverma005 <82157280+naveenverma005@users.noreply.github.com>
2021-05-13 22:16:37 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,18 @@
+#Variables for Security Group
+variable "sg_name" {
+  description = "Name of your Security Group"
+  type        = string
+  default     = "cfast-alb-sg"
+}
 
+variable "vpc_id" {
+  description = "Give your vpc id here"
+  type        = string
+  default     = "vpc-0cef057e0494947e4"
+}
+
+variable "sg_name_tag" {
+  description = "Tag name for your Security Group"
+  type        = string
+  default     = "cfast-alb-sg"
+}
