b596c451f8913a6c9ab7211292777c52cd9de4df
naveenverma005 <82157280+naveenverma005@users.noreply.github.com>
2021-05-13 22:14:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,38 @@
+module "security_group" {
+  source      = "https://gitlab.com/ot-client/docasap/tf-modules/security_group.git"
+  sg_name     = var.sg_name
+  vpc_id      = var.vpc_id
+  sg_name_tag = var.sg_name_tag 
+  sg_ingress  = [
+    {
+      description      = "For HTTPS request at port 443"
+      from_port        = 443
+      to_port          = 443
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      self             = false
+      security_groups  = []
+    },
+    {
+      description      = "For HTTP request at port 80"
+      from_port        = 80
+      to_port          = 80
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      self             = false
+      security_groups  = []
+    }
+  ]
 
+  sg_egress = [
+    {
+      description      = ""
+      from_port        = 0
+      to_port          = 0
+      protocol         = -1
+      cidr_blocks      = ["0.0.0.0/0"]
+      self             = false
+      security_groups  = []
+    }
+  ]
+}
