052a2d02ac37e369c2ad27be6de9703f8d3af765
naveenverma005 <82157280+naveenverma005@users.noreply.github.com>
2021-05-14 20:50:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -36,3 +36,20 @@ module "security_group" {
     }
   ]
 }
+  
+  
+module "alb_and_alb_listeners" {
+  source                                = "./alb"
+  alb_name                              = var.alb_name
+  internal_alb                          = var.internal
+  alb_security_groups_ids               = var.alb_security_groups_ids
+  subnets_id                            = var.subnets_id
+  enable_deletion_protection            = var.enable_deletion_protection
+  drop_invalid_header_fields            = var.drop_invalid_header_fields
+  idle_timeout                          = var.idle_timeout
+  alb_tags                              = var.alb_tags
+  alb_log_bucket                        = var.alb_log_bucket
+  alb_enable_logging                    = var.alb_enable_logging
+  prefix				= var.prefix
+}
+
