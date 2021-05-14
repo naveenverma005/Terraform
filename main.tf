9a6019db1b1a5984f3bdaca191ebee69e03b68f4
naveenverma005 <82157280+naveenverma005@users.noreply.github.com>
2021-05-14 21:22:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -36,7 +36,22 @@ module "security_group" {
     }
   ]
 }
-  
+
+module "target_group_and_target_group_attachment" {
+  source                = "./target_group"
+  target_group_details  = var.target_group_details
+  vpc_id                = var.vpc_id
+  healthy_threshold     = var.healthy_threshold
+  unhealthy_threshold   = var.unhealthy_threshold
+  timeout               = var.timeout
+  interval              = var.interval
+  health_check_path     = var.health_check_path
+  health_check_port     = var.health_check_port
+  deregistration_delay  = var.deregistration_delay
+  slow_start            = var.slow_start
+  target_ids            = var.target_ids
+  port                  = 80
+}
   
 module "alb_and_alb_listeners" {
   source                                = "./alb"
